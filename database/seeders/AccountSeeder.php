7941351ca416169df7027937089993e7e9c48d5b
ryosuke-horie <r-horie@mamiya-its.co.jp>
2024-03-18 12:50:04 +0900

    初期データ

--- database/seeders/AccountSeeder.php
+++ database/seeders/AccountSeeder.php
@@ -3,14 +3,17 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Account;
 
 class AccountSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        Account::create([
+            'name' => 'testuser',
+            'email' => 'test@examle.com',
+            'password' => Hash::make('password'),
+        ]);
     }
 }
