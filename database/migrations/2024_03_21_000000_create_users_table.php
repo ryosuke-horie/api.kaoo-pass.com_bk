4c4d27adeba3f512f045f7f06a72c7f5276c7fe6
ryosuke-horie <r-horie@mamiya-its.co.jp>
2024-03-23 11:15:40 +0900

    Modelにリレーションを張る

--- database/migrations/2024_03_21_000000_create_users_table.php
+++ database/migrations/2024_03_21_000000_create_users_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->unsignedBigInteger('account_id')->nullable();
+            $table->foreign('account_id')->references('id')->on('accounts')->onDelete('set null');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
