e21a1607e0d920d7486a5d5846e79725e12f80cb
ryosuke-horie <ryosuke.horie37@gmail.com>
2024-03-30 10:40:04 +0900

    登録処理の実装

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserPostRequest;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
@@ -31,25 +33,47 @@ class UserController extends Controller
     /**
      * ユーザーを登録
      */
-    public function store(): JsonResponse
+    public function store(UserPostRequest $request): JsonResponse
     {
-        // リクエストパラメータを取得
-        $params = request()->all();
         // ログイン中のアカウントIDを取得
-        $account_id = auth()->id();
-        // ユーザーを登録
-        $user = User::create([
-            'account_id' => $account_id,
-            'name' => $params['name'],
-            'email' => $params['email'],
-            'avatar_image' => $params['avatar_image'] ?? null,
-        ]);
+        $account_id = (int)auth()->id();
 
-        // アバター画像のURLを設定
-        if ($user->avatar_image) {
-            $user->avatar_image = Storage::disk('s3')->url($user->avatar_image);
+        // バリデーション済みデータの取得
+        $user = $request->validated();
+
+        // ファイルデータの型チェック
+        if (
+            !$user['avatar_image'] instanceof UploadedFile ||
+            !$user['image2'] instanceof UploadedFile ||
+            !$user['image3'] instanceof UploadedFile
+        ) {
+            return response()->json(['error' => 'Invalid file data'], 400);
         }
 
-        return response()->json($user);
+        // ファイル名の生成：ファイル名_uuid.拡張子
+        $avatarImageName = $user['avatar_image']->getClientOriginalName() . '_' . uniqid() . '.' . $user['avatar_image']->extension();
+        $image2Name = $user['image2']->getClientOriginalName() . '_' . uniqid() . '.' . $user['image2']->extension();
+        $image3Name = $user['image3']->getClientOriginalName() . '_' . uniqid() . '.' . $user['image3']->extension();
+
+        // ユーザー情報を登録
+        User::create([
+            'name' => $user['name'],
+            'email' => $user['email'],
+            'account_id' => $account_id,
+            'address' => $user['address'],
+            'phone' => $user['phone'],
+            'age' => $user['age'],
+            'avatar_image' => $avatarImageName,
+            'image2' => $image2Name,
+            'image3' => $image3Name,
+        ]);
+
+        // S3に画像を保存
+        Storage::disk('s3')->putFileAs('images', $user['avatar_image'], $avatarImageName);
+        Storage::disk('s3')->putFileAs('images', $user['image2'], $image2Name);
+        Storage::disk('s3')->putFileAs('images', $user['image3'], $image3Name);
+
+        // ステータスコード200でレスポンスを返却
+        return response()->json([], 200);
     }
 }
