2f1a97a7cb2a1f8c91982a8d2512ad493972ddef
ryosuke-horie <ryosuke.horie37@gmail.com>
2024-04-03 08:20:16 +0900

    支払いURL生成APIの仮実装（エラーあり）

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -153,6 +153,35 @@ class StripeController extends Controller
      */
     public function createCheckoutSession(Request $request): JsonResponse
     {
-        dd('test');
+        // ユーザーが選択した商品の価格IDを取得
+        $priceId = $request->input('stripe_price_id');
+
+        // ユーザーのstripe_account_idを取得
+        $stripeAccountId = $request->user()->account->stripe_account_id ?? null;
+
+        // 支払いページ作成オプション
+        $options = [
+            'payment_method_types' => ['card'],
+            'line_items' => [[
+                'price' => $priceId,
+                'quantity' => 1,
+            ]],
+            'subscription_data' => [
+                'application_fee_percent' => 10,
+            ],
+            'mode' => 'subscription',
+            'success_url' => 'https://kaoo-pass.com/success',
+            'cancel_url' => 'https://kaoo-pass.com/cancel',
+        ];
+
+        // stripe_account_idが存在する場合、オプションに追加
+        if ($stripeAccountId !== null) {
+            $options['stripe_account'] = $stripeAccountId;
+        }
+
+        // 支払いページ作成
+        $session = $this->stripe->checkout->sessions->create($options);
+
+        return response()->json(['session' => $session], 200);
     }
 }
