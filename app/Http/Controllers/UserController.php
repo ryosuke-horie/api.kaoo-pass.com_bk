9f084ef0047a451f59453e91d3633f1b4445e1c5
ryosuke-horie <ryosuke.horie37@gmail.com>
2024-04-09 22:24:10 +0900

    Actionに分割

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,8 +7,6 @@ use App\Models\User;
 use App\Usecases\User\IndexAction;
 use App\Usecases\User\StoreAction;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
 {
@@ -42,41 +40,8 @@ class UserController extends Controller
         // バリデーション済みデータの取得
         $user = $request->validated();
 
-        // ログイン中のアカウントIDを取得
-        $account_id = (int) auth()->id();
-
-        // ファイル名の生成：ファイル名_uuid.拡張子
-        $avatarImageName = $user['avatar_image']->getClientOriginalName().uniqid().'.'.$user['avatar_image']->extension();
-        $image2Name = $user['image2']->getClientOriginalName().uniqid().'.'.$user['image2']->extension();
-        $image3Name = $user['image3']->getClientOriginalName().uniqid().'.'.$user['image3']->extension();
-
-        // ユーザー情報を登録
-        User::create([
-            'name' => $user['name'],
-            'email' => $user['email'],
-            'account_id' => $account_id,
-            'address' => $user['address'],
-            'phone' => $user['phone'],
-            'age' => $user['age'],
-            'avatar_image' => $avatarImageName,
-            'image2' => $image2Name,
-            'image3' => $image3Name,
-        ]);
-
-        // S3に画像を保存
-        try {
-            Storage::disk('s3')->putFileAs('images', $user['avatar_image'], $avatarImageName);
-            Storage::disk('s3')->putFileAs('images', $user['image2'], $image2Name);
-            Storage::disk('s3')->putFileAs('images', $user['image3'], $image3Name);
-            Log::info('Images uploaded successfully');
-        } catch (\League\Flysystem\UnableToWriteFile $exception) {
-            Log::error('S3 upload error: '.$exception->getMessage());
-
-            return response()->json(['error' => 'File upload failed'], 500);
-        }
-
-        // ステータスコード200でレスポンスを返却
-        return response()->json([], 200);
+        // storeActionを呼び出し
+        return ($this->storeAction)($user);
     }
 
     /**
