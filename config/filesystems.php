8cc918a67a98936e594ee770a76e6bf1b413943d
ryosuke-horie <ryosuke.horie37@gmail.com>
2024-04-19 08:40:12 +0900

    Laravel11アプリケーション構造追従

--- config/filesystems.php
+++ config/filesystems.php
@@ -9,10 +9,9 @@ return [
     |
     | Here you may specify the default filesystem disk that should be used
     | by the framework. The "local" disk, as well as a variety of cloud
-    | based disks are available to your application. Just store away!
+    | based disks are available to your application for file storage.
     |
     */
-    'public' => env('FILESYSTEM_DISK', 'public_local'),
 
     'default' => env('FILESYSTEM_DISK', 'local'),
 
@@ -21,9 +20,9 @@ return [
     | Filesystem Disks
     |--------------------------------------------------------------------------
     |
-    | Here you may configure as many filesystem "disks" as you wish, and you
-    | may even configure multiple disks of the same driver. Defaults have
-    | been set up for each driver as an example of the required values.
+    | Below you may configure as many filesystem disks as necessary, and you
+    | may even configure multiple disks for the same driver. Examples for
+    | most supported storage drivers are configured here for reference.
     |
     | Supported Drivers: "local", "ftp", "sftp", "s3"
     |
@@ -37,7 +36,7 @@ return [
             'throw' => false,
         ],
 
-        'public_local' => [
+        'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
@@ -45,18 +44,8 @@ return [
             'throw' => false,
         ],
 
-        // 's3' => [
-        //     'driver' => 's3',
-        //     'key' => env('AWS_ACCESS_KEY_ID'),
-        //     'secret' => env('AWS_SECRET_ACCESS_KEY'),
-        //     'token' => env('AWS_SESSION_TOKEN'),
-        //     'region' => env('AWS_DEFAULT_REGION'),
-        //     'bucket' => env('AWS_BUCKET'),
-        //     'url' => env('AWS_URL'),
-        //     'endpoint' => env('AWS_ENDPOINT'),
-        //     'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-        //     'throw' => true,
-        // ],
+        // @Note: Laravel11.x 対応から除外（s3, s3_public）
+        // デフォルトから変更しているため
 
         's3' => [
             'driver' => 's3',
